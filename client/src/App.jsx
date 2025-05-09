601c271e4dd70dc6c6dfc756e1aea064bf5df1c8
dsjramesh7 <rameshdussa07@gmail.com>
2025-05-09 18:04:00 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button className="bg-red-500">Click me</Button>
+    </div>
+  );
 };
 
 export default App;
